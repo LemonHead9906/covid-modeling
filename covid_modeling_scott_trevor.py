fc255786716d2aa4b41b7bf16c2a6f511d7bf96c
LemonHead9906 <49240006+LemonHead9906@users.noreply.github.com>
2020-05-01 08:34:23 -0400

    Version 0.2
    Added a print statement that explains the rate at which the number of infections doubles.

--- covid_modeling_scott_trevor.py
+++ covid_modeling_scott_trevor.py
@@ -1,6 +1,6 @@
 Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> # Simple COVID-19 Exponential Growth Simulator Template -- Trevor S -- 5/1/2020 -- 8:26 AM -- 0.1 
+>>> # Simple COVID-19 Exponential Growth Simulator Template -- Trevor S -- 5/1/2020 -- 8:33 AM -- 0.2 
 
 import time
 
@@ -9,14 +9,9 @@ current_day = 0
 days_sim = 0
 num_deaths_ppl = 0
 
-# Create a variable here that will store the number of infected people, assign it a value of 0.  
-# Create a variable here that will store the current day number, assign it a value of 0. (i.e. the first day is day 0, the second day is day 1, etc.)
-# Create a variable here that will store the total number of days to simluate, assign it a value of 0. (i.e. how many days worth of growth will you simulate?)
-# Create a variable here that will store the number of people that will die from COVID-19, assign it a value of 0.  
+print("This program will model potential Covid-19 infections and deaths in the U.S. Right now, the number of infections doubles evry six days.\n")
+time.sleep(4)
 
-# Use a print() statement to explain that the number of infections doubles every six days.
-
-# Use time.sleep() to pause a few seconds so the user can read your print() statement.
 
 # Use an input() statement to have the user enter the CURRENT NUMBER OF INFECTED CASES.  Make sure it's an INT.
 # Use an input() statement to have the user enter the NUMBER OF DAYS TO SIMULATE.  Make sure it's an INT.
